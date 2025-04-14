97b47b23caeb255a06e2f888ef89408e281058ed
danielho774 <hodk@usc.edu>
2025-04-13 18:01:16 -0700

    Added baseline Controller classes, no functionality yet.

--- backend/src/main/java/com/app/project/controller/CommentController.java
+++ backend/src/main/java/com/app/project/controller/CommentController.java
@@ -1,3 +1,53 @@
+package com.app.project.controller;
+
+import com.app.project.model.Comment;
+import com.app.project.service.CommentService;  
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/comments")
 public class CommentController {
-    
+
+    // @Autowired
+    // private CommentService commentService;
+
+    // @PostMapping
+    // public ResponseEntity<Comment> saveComment(@RequestBody Comment comment){
+    //     return new ResponseEntity<Comment>(commentService.saveComment(comment), HttpStatus.CREATED);
+    // }
+    // //GetAll Rest Api
+    // @GetMapping
+    // public List<Comment> getAllComment(){
+    //     return commentService.getAllComment();
+    // }
+
+    // //Get by Id Rest Api
+    // @GetMapping("{id}")
+    // // localhost:8080/api/comments/1
+    // public ResponseEntity<Comment> getCommentById(@PathVariable("id") long commentID){
+    //     return new ResponseEntity<Comment>(commentService.getCommentById(commentID),HttpStatus.OK);
+    // }
+
+    // //Update Rest Api
+    // @PutMapping("{id}")
+    // public ResponseEntity<Comment> updateComment(@PathVariable("id") long id,
+    //                                                @RequestBody Comment comment){
+    //     return new ResponseEntity<Comment>(commentService.updateComment(comment,id),HttpStatus.OK);
+    // }
+
+    // //Delete Rest Api
+    // @DeleteMapping("{id}")
+    // public ResponseEntity<String> deleteComment(@PathVariable("id") long id){
+    //     //delete Comment from db
+    //     commentService.deleteComment(id);
+    //     return new ResponseEntity<String>("Comment deleted Successfully.",HttpStatus.OK);
+    // }
+
 }
--- backend/src/main/java/com/app/project/controller/TaskController.java
+++ backend/src/main/java/com/app/project/controller/TaskController.java
@@ -1,3 +1,53 @@
+package com.app.project.controller;
+
+import com.app.project.model.Task;
+import com.app.project.service.TaskService;  
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
-    
+
+    // @Autowired
+    // private TaskService taskService;
+
+    // @PostMapping
+    // public ResponseEntity<Task> saveTask(@RequestBody Task task){
+    //     return new ResponseEntity<Task>(taskService.saveTask(task), HttpStatus.CREATED);
+    // }
+    // //GetAll Rest Api
+    // @GetMapping
+    // public List<Task> getAllTask(){
+    //     return TaskService.getAllTask();
+    // }
+
+    // //Get by Id Rest Api
+    // @GetMapping("{id}")
+    // // localhost:8080/api/tasks/1
+    // public ResponseEntity<Task> getTaskById(@PathVariable("id") long taskID){
+    //     return new ResponseEntity<Task>(taskService.getTaskById(taskID),HttpStatus.OK);
+    // }
+
+    // //Update Rest Api
+    // @PutMapping("{id}")
+    // public ResponseEntity<Task> updateTask(@PathVariable("id") long id,
+    //                                                @RequestBody Task task){
+    //     return new ResponseEntity<Task>(taskService.updateTask(task,id),HttpStatus.OK);
+    // }
+
+    // //Delete Rest Api
+    // @DeleteMapping("{id}")
+    // public ResponseEntity<String> deleteTask(@PathVariable("id") long id){
+    //     //delete Task from db
+    //     taskService.deleteTask(id);
+    //     return new ResponseEntity<String>("Task deleted Successfully.",HttpStatus.OK);
+    // }
+
 }

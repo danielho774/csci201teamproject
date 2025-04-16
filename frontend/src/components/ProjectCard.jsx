ed4c8bde5e0cfc5fc760a3e67786b51348147757
Allison Lee <allisonlee572@gmail.com>
2025-04-16 15:29:56 -0700

    Create ProjectCard.jsx

---
+++ frontend/src/components/ProjectCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProjectCard() {
+    return (
+        <div className = "projectCard">
+            <h1>This is the project name</h1>
+            <p>This is the project info (maybe)</p>
+        </div>
+    )
+}
+
+export default ProjectCard; 

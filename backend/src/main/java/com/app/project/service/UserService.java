5dbfca7a8eec2fd2c6d878ec15a6cb5662b7ee3c
Danah <dyun2022@gmail.com>
2025-04-15 19:08:33 -0700

    updated

--- backend/src/main/java/com/app/project/service/UserService.java
+++ backend/src/main/java/com/app/project/service/UserService.java
@@ -1,3 +1,11 @@
-public class UserService {
-    
+package com.app.project.service; 
+
+import com.app.project.model.User; 
+import java.util.List; 
+
+public interface UserService {
+    User saveUser(User user); 
+    List<User> getAllUsers(); 
+    User getUserByID(long id); 
+    void deleteUser(long id); 
 }

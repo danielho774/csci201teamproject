a52d8728040352cf1be146f1199997ecba06d6e0
Danah Yun <dnyun@usc.edu>
2025-04-15 18:07:20 -0700

    Update TaskAssignments.java

--- backend/src/main/java/com/app/project/model/TaskAssignments.java
+++ backend/src/main/java/com/app/project/model/TaskAssignments.java
@@ -1,1 +1,48 @@
+package com.app.project.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TaskAssignments")
+public class TaskAssignments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assignment_id")
+    private int assignmentID;
+
+    @ManyToOne 
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task; 
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false) 
+    private ProjectMember member; 
+
+    public TaskAssignments(Task task, ProjectMember member) {
+        this.task = task; 
+        this.member = member; 
+    }
+
+    // Getters and Setters 
+    public int getAssignmentID() {
+        return assignmentID; 
+    }
+    public void setAssignmentID(int assignmentID) {
+        this.assignmentID = assignmentID; 
+    }
+
+    public Task getTask() {
+        return task; 
+    }
+    public void setTask(Task task) {
+        this.task = task; 
+    }
+
+    public ProjectMember getMember() {
+        return member; 
+    }
+    public void setMember(ProjectMember member) {
+        this.member = member; 
+    }
+}
 

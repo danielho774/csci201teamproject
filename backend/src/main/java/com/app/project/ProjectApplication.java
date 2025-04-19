53fd9b0eb0d357004da5ea336321d7507d3285fc
danielho774 <hodk@usc.edu>
2025-04-19 12:12:58 -0700

    Updates to Application file

--- backend/src/main/java/com/app/project/ProjectApplication.java
+++ backend/src/main/java/com/app/project/ProjectApplication.java
@@ -1,10 +1,5 @@
 package com.app.project;
 
-import java.beans.BeanProperty;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,13 +8,6 @@ public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			// Your custom logic here
-			System.out.println("Hello, World!");
-		};
+		System.out.println("server started successfully.");
 	}
 }

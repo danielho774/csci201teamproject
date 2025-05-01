ab72192cd3ea33534f2aa4a96111527a940b46e3
LiorUSC <153338712+LiorUSC@users.noreply.github.com>
2025-04-30 19:23:01 -0700

    added return to get rid of 404 error

--- backend/src/main/java/com/app/project/ProjectApplication.java
+++ backend/src/main/java/com/app/project/ProjectApplication.java
@@ -4,14 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.app.project.service.impl", "com.app.project.service"})
 @EntityScan(basePackages = "com.app.project.model")
+@RestController
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 		System.out.println("server started successfully.");
+	}
+
+	//added to get rid of 404 error
+	// when we hit the url http://localhost:8080/
+	@GetMapping
+	public String Sucess() {
+		return "Welcome to the project";
 	}
 }

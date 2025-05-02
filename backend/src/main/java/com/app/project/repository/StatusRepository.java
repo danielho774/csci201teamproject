dbba50c4a2120398b248bf4bf624520a3a8f3cc1
Danah Yun <dnyun@usc.edu>
2025-05-02 04:47:27 -0700

    Update StatusRepository.java

--- backend/src/main/java/com/app/project/repository/StatusRepository.java
+++ backend/src/main/java/com/app/project/repository/StatusRepository.java
@@ -4,6 +4,6 @@ import com.app.project.model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StatusRepository extends JpaRepository<TaskStatus, Integer> {
-    
+   TaskStatus findByStatusName(String statusName); 
 }
 

7b7e59015c46978c9b2a42eceecbd87414827ee4
Danah <dyun2022@gmail.com>
2025-05-02 05:08:00 -0700

    updated

--- backend/src/main/java/com/app/project/service/impl/StatusServiceImpl.java
+++ backend/src/main/java/com/app/project/service/impl/StatusServiceImpl.java
@@ -3,6 +3,9 @@ package com.app.project.service.impl;
 import com.app.project.model.TaskStatus;
 import com.app.project.repository.StatusRepository;
 import com.app.project.service.TaskStatusService;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +27,14 @@ public class StatusServiceImpl implements TaskStatusService {
     @Override
     public List<TaskStatus> getAllStatus() {
         return StatusRepository.findAll();
+    }
+
+    @PostConstruct
+    public void init() {
+        if (StatusRepository.count() == 0) {
+            StatusRepository.save(new TaskStatus("Incomplete")); 
+            StatusRepository.save(new TaskStatus("In Progress")); 
+            StatusRepository.save(new TaskStatus("Complete")); 
+        }
     }
 }

da28166c42460bcd5865391ce55c0645ae7201c9
Danah <dyun2022@gmail.com>
2025-05-02 13:41:20 -0700

    navigation update

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom'; 
 import styles from './NavigationBar.module.css'; 
 
 // Receive isLoggedIn and onLogout as props
-function NavigationBar({ isLoggedIn, onLogout }) {
+function NavigationBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+
+  useEffect(() => {
+    const userID = localStorage.getItem('userID'); 
+    if (userID) {
+      setIsLoggedIn(true); 
+    }
+    else {
+      setIsLoggedIn(false); 
+    }
+  }); 
+
+  const handleLogout = () => {
+    localStorage.removeItem('userID'); 
+    setIsLoggedIn(false); 
+  }; 
+
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navList}>
@@ -11,27 +28,30 @@ function NavigationBar({ isLoggedIn, onLogout }) {
          
           <Link to="/" className={styles.navLink}>Project Hub</Link>
         </li>
-        <li className={styles.navItem}>
-          <Link to="/tasks" className={styles.navLink}>Individual Tasks</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link to="/create" className={styles.navLink}>Create Project</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link to="/availability" className={styles.navLink}>Availability</Link>
-        </li>
-        {/* Conditional Login/Logout */}
-        <li className={styles.navItem}>
-          {isLoggedIn ? (
-            <button onClick={onLogout} className={`${styles.navLink} ${styles.logoutButton}`}>Logout</button>
-          ) : (
-            <Link to="/login" className={styles.navLink}>Login</Link>
-          )}
-        </li>
-       
+
+        {isLoggedIn ? (
+          <>
+            <li className={styles.navItem}>
+              <Link to="/tasks" className={styles.navLink}>Individual Tasks</Link>
+           </li>
+          <li className={styles.navItem}>
+            <Link to="/create" className={styles.navLink}>Create Project</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link to="/availability" className={styles.navLink}>Availability</Link>
+          </li>
+          <li className={styles.navItem}>
+            <button onClick={handleLogout} className = {`${styles.navLink} ${styles.logoutButton}`}>Logout</button>
+          </li>
+        </>
+        ) : (
+          <li className = {styles.navItem}>
+            <Link to = "/login" className = {styles.navLink}>Login</Link>
+          </li>
+        )}
       </ul>
     </nav>
-  );
+  ); 
 }
 
 export default NavigationBar; 

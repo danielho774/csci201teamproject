e2a6a68f77a75259cc10448ea580a1bd1db8cca0
danielho774 <hodk@usc.edu>
2025-05-02 16:28:43 -0700

    updates to project hub for users who are logged in

--- frontend/src/pages/ProjectHubPage.jsx
+++ frontend/src/pages/ProjectHubPage.jsx
@@ -4,26 +4,57 @@ import styles from './ProjectHubPage.module.css';
 import ProjectCard from '../components/ProjectCard';
 
 export default function ProjectHubPage() {
-  
-  const projects = [
-    { id: 'proj1', title: 'Project1' },
-    { id: 'proj2', title: 'Project2' },
-    { id: 'proj3', title: 'Project3' },
-    { id: 'proj4', title: 'Project4' },
-    { id: 'proj5', title: 'Project5' },
-    { id: 'superlong', title: 'Insert Super Long Project Name So I Know To Take Care of This Case' },
-    { id: 'proj7', title: 'Project7' },
-  
-  ];
+  //create a function to fetch projects from the backend
+  // and display them in the project hub page
+  const [projects, setProjects] = React.useState([]);
+  const [noProjects, setNoProjects] = React.useState(false);
+
+  const fetchProjects = async () => {
+    try {
+      const userID = localStorage.getItem('userID');
+      const response = await fetch(`http://localhost:8080/api/users/${userID}/projects`, {
+        method: 'GET', 
+        headers: {
+          'Content-Type': 'application/json', 
+        }, 
+      }); 
+    
+      if (!response.ok) {
+        throw new Error('Invalid Request.'); 
+      }
+
+      if (response.status === 204) {
+        console.log('No projects found for this user.'); 
+        setNoProjects(true);
+        return; 
+      }
+      const projects = await response.json();
+      console.log('Projects: ', projects);
+      setProjects(projects);
+    }
+    catch(error) {
+      console.error('Fetch failed: ', error.message); 
+      alert('Fetch failed: ' + error.message); 
+    }
+  }
+
+  // Call the fetchProjects function to fetch projects from the backend
+  React.useEffect(() => {
+    fetchProjects();
+  }, []);
+
 
   return (
     <div className = {styles['project-hub']}>
       <h2>Project Hub</h2>
       <div className={styles['grid']}>
         {projects.map(project => (
-          <ProjectCard key={project.id} projectId={project.id} project-title={project.title} />
+          <ProjectCard key={project.id} projectId={project.projectID} project-title={project.projectName} />
         ))}
       </div>
+      {noProjects === true &&
+        <div>if true show</div>
+      }
     </div>
   );
 }
